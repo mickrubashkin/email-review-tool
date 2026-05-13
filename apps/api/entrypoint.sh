ec3cd1c6a228e9cfbc5fe54c0f9427fbb6c6974d
Mick Ruabshkin <mikhail.rubashkin@gmail.com>
2026-05-13 12:42:36 +0200

    add some docs, update entrypoin

--- apps/api/entrypoint.sh
+++ apps/api/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-set -e
+set -ex
 
+echo "Running migrations"
 goose -dir /app/db/migrations postgres "$DATABASE_URL" up
+
+echo "Running seed"
 /app/seed
+
+echo "Starting server"
 exec /app/server
